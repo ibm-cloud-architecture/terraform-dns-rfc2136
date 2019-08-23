c9488dd6c696eed1b3db1092924b0b2d8c0c77ff
Jeffrey Kwong <jkwong@ca.ibm.com>
2019-08-23 16:01:33 -0400

    add a records, srv records, and ptr records for nodes

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "dns" {
   }
 }
 
+
 resource "dns_a_record_set" "node_a_record" {
   count = "${length(var.node_ips)}"
 
@@ -17,12 +18,45 @@ resource "dns_a_record_set" "node_a_record" {
   ttl = "${var.record_ttl}"
 }
 
+resource "dns_ptr_record" "node_ptr_record" {
+  count = "${var.create_node_ptr_records ? length(var.node_ips) : 0}"
+
+  zone = "${format("%s.in-addr.arpa.", join(".", reverse(slice(split(".", element(var.node_ips, count.index)), 0, 3))))}"
+  name = "${element(split(".", element(var.node_ips, count.index)), 3)}"
+  ptr = "${element(var.node_hostnames, count.index)}.${var.zone_name}"
+
+  ttl = "${var.record_ttl}"
+}
+
 resource "dns_a_record_set" "other_a_record" {
-  count = "${length(keys(var.a_records))}"
+  count = "${length(var.a_records)}"
 
   zone = "${var.zone_name}"
   name = "${element(keys(var.a_records), count.index)}"
 
   addresses = ["${element(values(var.a_records), count.index)}"]
   ttl = "${var.record_ttl}"
+}
+
+resource "dns_srv_record_set" "srv_records" {
+  count = "${length(var.srv_records)}"
+
+  zone = "${var.zone_name}"
+  name = "${element(var.srv_records, count.index)}"
+
+  dynamic "srv" {
+    for_each = matchkeys(
+      keys(var.srv_record_targets), 
+      values(var.srv_record_targets), 
+      list(element(var.srv_records, count.index)))
+
+    content {
+      priority = 0
+      weight = 10
+      target = "${format("%s.", element(split(":", srv.value), 0))}"
+      port = "${element(split(":", srv.value), 1)}"
+    }
+  }
+    
+
 }
--- variables.tf
+++ variables.tf
@@ -13,6 +13,20 @@ variable "a_records" {
   default = {}
 }
 
+variable "srv_records" {
+  type = "list"
+  default = []
+}
+
+variable "srv_record_targets" {
+  type = "map"
+  default = {}
+}
+
+variable "create_node_ptr_records" {
+  default = false
+}
+
 variable "dns_server" {}
 variable "key_name" {}
 variable "key_algorithm" {}
